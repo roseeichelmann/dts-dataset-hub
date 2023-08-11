3806cdd3414f276e19e55ffe83e34587d052c82c
rose <rose.eichelmann@gmail.com>
2023-08-11 13:57:41 -0500

    delete unused files, add header icon

--- dataset-hub/pages/index.js
+++ dataset-hub/pages/index.js
@@ -1,37 +1,33 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+
 // import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 import NavTile from "../components/NavTile";
 // import PageHead from "../components/PageHead";
 
 const DESCRIPTION =
-  "Dashboards and public datasets curated by City of Austin Transportation and Public Works";
+  "Datasets published and maintained by the Transportation and Public Works Department's Data and Technology Services Division.";
 
 export default function Home() {
   return (
-    <>
-      {/* <PageHead
-        title="Austin Transportation Data and Performance Hub"
-        description={DESCRIPTION}
-        pageRoute="/"
-        imageRoute="/assets/home-thumbnail.png"
-      /> */}
-      <div className="wrapper">
-        <Nav isHome />
-        <Container className="main">
-          <Row>
-            <Col xs={12} className="pt-5 text-center">
-              <h1 className="text-primary fw-bold">Data & Performance Hub</h1>
-            </Col>
-          </Row>
-          <Row className="mb-2 border-bottom">
-            <Col xs={12} className="text-center">
-              <p className="text-muted">{DESCRIPTION}</p>
-            </Col>
-          </Row>
-          {/* <Row className="text-dts-4 mb-4">
+    <div className="wrapper">
+      <Nav isHome>
+        <Row></Row>
+      </Nav>
+      <Container className="main">
+        <Row>
+          <Col xs={12} className="pt-5 text-center">
+            <h1 className="text-primary fw-bold">Dataset Hub</h1>
+          </Col>
+        </Row>
+        <Row className="mb-2 border-bottom">
+          <Col xs={12} className="text-center">
+            <p className="text-muted">{DESCRIPTION}</p>
+          </Col>
+        </Row>
+        {/* <Row className="text-dts-4 mb-4">
             {PAGES.map((page) => (
               <Col
                 key={page.href}
@@ -44,9 +40,8 @@ export default function Home() {
               </Col>
             ))}
           </Row> */}
-        </Container>
-        {/* <Footer /> */}
-      </div>
-    </>
+      </Container>
+      {/* <Footer /> */}
+    </div>
   );
 }
