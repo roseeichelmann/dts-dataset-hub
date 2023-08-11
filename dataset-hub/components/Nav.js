ac472bae213176677d185ad96bff8adc23159a31
rose <rose.eichelmann@gmail.com>
2023-08-11 13:42:23 -0500

    recreate next.js app, rearrange file architecture

---
+++ dataset-hub/components/Nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Image, Nav as BsNav, Navbar, Container } from "react-bootstrap";
+import Link from "next/link";
+
+export default function Nav({ isHome }) {
+  return (
+    <>
+      <Navbar
+        expand="lg"
+        className={`py-1 ${isHome ? "nav-shadow" : "border"}`}
+      >
+        <Container fluid key="nav-container">
+          <Link href="/" passHref>
+            <Navbar.Brand
+              style={{ cursor: "pointer" }}
+              className="ps-2 me-auto"
+            >
+              <div className="d-flex align-items-center">
+                <div className="me-1">
+                  {/* <Image
+                    height={60}
+                    src="/assets/2023_austin_transportation_public_works_branding_guide_royal.png"
+                    alt="Transportation and Public Works wordmark including the City of Austin seal"
+                  /> */}
+                </div>
+              </div>
+            </Navbar.Brand>
+          </Link>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
